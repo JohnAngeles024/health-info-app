1f294cfd8ecf788783742404f4452b9a9c96a6c2
JohnAngeles024 <86285092+JohnAngeles024@users.noreply.github.com>
2021-07-26 17:27:01 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Health Declaration using React App
+
+Watch the full lesson in pinoyfreecoder youtube channel https://youtu.be/kYsgJPJZJIg
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+import Navbar from './components/navbar.component';
+import HealthList from './components/health-list.component';
+import EditHealth from './components/edit-health.component';
+import CreateHealth from './components/create-health.component';
+
+function App() {
+  return (
+    <Router>
+       <Navbar/>
+       <br/>
+      <Route path="/" exact component={HealthList} />
+      <Route path="/edit/:id" component={EditHealth} />
+      <Route path="/create" component={CreateHealth} />
+    </Router>
+  );
+}
+
+export default App;
